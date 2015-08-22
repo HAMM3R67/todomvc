f9558194ca32398bf9e8de7225e34929b3973ac7
HAMM3R67 <tcschauer@gmail.com>
2015-08-22 12:57:46 -0600

    adjusting the different types of ways to update data

--- js/controllers/todoCtrl.js
+++ js/controllers/todoCtrl.js
@@ -12,18 +12,18 @@ todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, $firebaseArr
 
     //LOAD DATA. This really should be done through a service, but it works.
     //1. Non persistent way
-    $scope.todos = [
-        {title: 'An example todo item', completed: false},
-        {title: 'Another example one', completed: true}
-    ];
+    // $scope.todos = [
+    //     {title: 'An example todo item', completed: false},
+    //     {title: 'Another example one', completed: true}
+    // ];
     //2. Firebase manual way
-	//$scope.todos = $firebaseArray(fireRef);
+	// $scope.todos = $firebaseArray(fireRef);
 
     //3. Firebase automatic way
-    /*
+    
     var obj = $firebaseObject(fireRef);
     obj.$bindTo($scope, 'todos');
-    */
+    
 
 	$scope.newTodo = '';
 	$scope.editedTodo = null;
@@ -55,28 +55,28 @@ todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, $firebaseArr
 		}
         //ADD A NEW ITEM TO THE LIST
         //1. Non persistent way
-        $scope.todos.push({
-            title: newTodo,
-            completed: false
-        });
+        // $scope.todos.push({
+        //     title: newTodo,
+        //     completed: false
+        // });
 
         //2. Firebase way
-        /*
-        $scope.todos.$add({
-			title: newTodo,
-			completed: false
-		});
-		*/
+        
+        // $scope.todos.$add({    //$add is firebases version of .push after a firebase array has been created.
+		// 	title: newTodo,
+		// 	completed: false
+		// });
+		
         //3. Firebase automatic way
         /*Slightly different from non persistent way since $scope.todos is an object
         when we're doing it this way. But we simply change it non persistently and later
         the library automatically detects the change and then updates Firebase */
-        /*
+        
         $scope.todos[Date.now()] = {
             title: newTodo,
             completed: false
         };
-        */
+        
 
 		$scope.newTodo = '';
 	};
@@ -95,7 +95,7 @@ todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, $firebaseArr
             /* Do nothing since the the variable "todo" has already been modified and will be noticed by angular's two way binding*/
 
 			//2. Firebase way
-			//$scope.todos.$save(todo);
+			// $scope.todos.$save(todo);  //$save tells firebase to save a change that has been made
 
             //3. Firebase Automatic Way
             /* Do nothing. The change will be automatically detected and saved */
@@ -113,20 +113,20 @@ todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, $firebaseArr
 	$scope.removeTodo = function (todo) {
         //REMOVE THE ITEM
         //1. Non persistent way
-        $scope.todos.splice($scope.todos.indexOf(todo), 1);
+        // $scope.todos.splice($scope.todos.indexOf(todo), 1);
 
         //2. Firebase way
-		//$scope.todos.$remove(todo);
+		// $scope.todos.$remove(todo);  //$remove is another firebase method
 
         //3. Firebase Automatic way
         /* Very similar to non persistent way except its an object, not an array */
-        /*
+        
         angular.forEach($scope.todos, function(thisTodo, key){
             if(todo === thisTodo){
                 delete $scope.todos[key];
             }
         })
-        */
+        
 	};
 
 	$scope.clearCompletedTodos = function () {
@@ -146,7 +146,7 @@ todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, $firebaseArr
             //1. Non persistent way
                 /* Do nothing since we've already marked it completed */
             //2. Firebase way
-            //$scope.todos.$save(todo);
+            // $scope.todos.$save(todo);
             //3. Automatic way
                 /* Also do nothing. Will be detected as changed and then saved */
 
